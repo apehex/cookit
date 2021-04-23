16917dcbda55ac255664026e9e7e74a9a85db5c3
apehex <apehex@protonmail.com>
2021-04-23 16:11:49 +0000

    Extract & rank all values by occurence

--- operations/calculate-statistics.sh
+++ operations/calculate-statistics.sh
@@ -1,5 +1,11 @@
 # list and rank the cookies by frequency of occurence
 grep -rai '"name"' cookies/ |
-  perl -ne 'm#"name":\s*"(.+)",#g && print $1."\n"' |
+  perl -ne 'm/"name":\s*"(.+?)",/g && print $1."\n"' |
   sort --version-sort | uniq -c | sort -k 1,2 -r |
   perl -pe 's/^\s*//g' > metadata/names.txt
+
+# list cookie values
+grep -rai '"value"' cookies/ |
+  perl -ne 'm/"value":\s*"(.+?)",/g && print $1."\n"' |
+  sort --version-sort | uniq -c | sort -k 1,2 -r |
+  perl -pe 's/^\s*//g' > metadata/values.txt
