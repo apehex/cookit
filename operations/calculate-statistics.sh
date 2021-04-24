d55e445ce361e5d0e49ae7d3cbc32f4a1aac69d0
apehex <apehex@protonmail.com>
2021-04-24 10:53:26 +0000

    Tokenize the cookie values to parse into meaningful blocks

--- operations/calculate-statistics.sh
+++ operations/calculate-statistics.sh
@@ -1,3 +1,7 @@
+# consider removing:
+# - timestamps (1619173596)
+# - dates (especially before spliting since it will produce tokens like "04" etc)
+
 # list and rank the cookies by frequency of occurence
 grep -rai '"name"' cookies/ |
   perl -ne 'm/"name":\s*"(.+?)",/g && print $1."\n"' |
@@ -7,5 +11,11 @@ grep -rai '"name"' cookies/ |
 # list cookie values
 grep -rai '"value"' cookies/ |
   perl -ne 'm/"value":\s*"(.+?)",/g && print $1."\n"' |
-  sort --version-sort | uniq -c | sort -k 1,2 -r |
+  sort | uniq -c | sort -k 1,2 -n -r |
   perl -pe 's/^\s*//g' > metadata/values.txt
+
+# tokenize
+grep -rai '"value"' cookies/ |
+  perl -ne 'm/"value":\s*"(.+?)",/g && print $1."\n"' |
+  tok |
+  sort | uniq -c | sort -k 1,2 -n -r > metadata/tokens.txt
