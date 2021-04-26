9abbfaad0e8304cfe36e312c88a479e5c1ab240a
apehex <apehex@protonmail.com>
2021-04-26 17:51:25 +0000

    Sort the names by occurence, interpreting it as a numeric value

--- operations/calculate-statistics.sh
+++ operations/calculate-statistics.sh
@@ -5,7 +5,7 @@
 # list and rank the cookies by frequency of occurence
 grep -rai '"name"' cookies/ |
   perl -ne 'm/"name":\s*"(.+?)",/g && print $1."\n"' |
-  sort --version-sort | uniq -c | sort -k 1,2 -r |
+  sort --version-sort | uniq -c | sort -k 1,2 -n -r |
   perl -pe 's/^\s*//g' > metadata/names.txt
 
 # list cookie values
